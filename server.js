98b57fe2f852425ccc118ba3c7f2e5b3fc0abaa5
Ezequiel Weiss <38471904+ezeweiss@users.noreply.github.com>
2025-02-04 09:45:25 -0300

    cambio cors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 // Configurar el transporte de correo
